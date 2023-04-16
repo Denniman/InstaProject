80ad6329e014f0072b5b9b04342cff2053b27d47
denniman <dennimanadams@gmail.com>
2023-04-16 07:03:43 +0100

    add global theme

---
+++ src/theme/fonts.ts
@@ -0,0 +1,19 @@
+import {TextStyle} from 'react-native';
+export const fontsColor = {
+  white: '#fff',
+  black: '#000',
+  blue: '#0000FF',
+  gray: '#A1A5AD',
+  RED_COLOR: '#f41515',
+  BORDER_GRAY: '#e9eaec',
+  accent: 'rgb(255, 48, 64)',
+  MILK_WHITE_COLOR: '#eeeeef',
+  MEDIUMSLATEBLUE_COLOR: '#0096FF',
+};
+
+export const fontWeight: {[key: string]: TextStyle['fontWeight']} = {
+  thin: '400',
+  bold: '500',
+  xBold: '700',
+  normal: 'normal',
+};
