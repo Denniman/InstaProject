e0e2587080df67459032b57545ca311605697597
denniman <dennimanadams@gmail.com>
2023-04-17 05:14:53 +0100

    add carousel component

---
+++ src/components/Carousel/Carousel.tsx
@@ -0,0 +1,82 @@
+/* eslint-disable react-native/no-inline-styles */
+import React, {useState, useRef} from 'react';
+import {
+  View,
+  Image,
+  FlatList,
+  ViewToken,
+  ViewabilityConfig,
+  useWindowDimensions,
+} from 'react-native';
+
+import {fontsColor} from '../../theme';
+import {DoublePress} from '../../hooks';
+
+interface ICoursel {
+  images: string[];
+  onDoublePress: () => void;
+}
+export const Carousel = ({images, onDoublePress}: ICoursel) => {
+  const {width} = useWindowDimensions();
+  const [activeImageIndex, setActiveImageIndex] = useState<number | null>();
+
+  const onViewableItemsChanged = useRef(
+    ({viewableItems}: {viewableItems: ViewToken[]}) => {
+      if (viewableItems.length > 0) {
+        setActiveImageIndex(viewableItems[0].index);
+      }
+    },
+  );
+
+  const viewabilityConfig: ViewabilityConfig = {
+    itemVisiblePercentThreshold: 51,
+  };
+
+  return (
+    <View>
+      <FlatList
+        horizontal
+        pagingEnabled
+        onViewableItemsChanged={onViewableItemsChanged.current}
+        viewabilityConfig={viewabilityConfig}
+        data={images}
+        renderItem={({item}) => (
+          <DoublePress onDoublePress={onDoublePress}>
+            <Image
+              source={{
+                uri: item,
+              }}
+              style={{width, aspectRatio: 1}}
+            />
+          </DoublePress>
+        )}
+      />
+
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+          position: 'absolute',
+          bottom: -15,
+          width: '100%',
+        }}>
+        {images.map((_, index) => (
+          <View
+            key={index.toString()}
+            style={{
+              width: 10,
+              borderRadius: 5,
+              aspectRatio: 1,
+              marginHorizontal: 4,
+              backgroundColor:
+                activeImageIndex === index
+                  ? fontsColor.MEDIUMSLATEBLUE_COLOR
+                  : fontsColor.gray,
+            }}
+          />
+        ))}
+      </View>
+    </View>
+  );
+};
